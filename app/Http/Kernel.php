83e0ad2aeb546f684e3e1013d0bc0b413fefd262
Dejoyag782 <dejoyag782@gmail.com>
2025-06-02 18:38:55 +0800

    BU-21 Added Docker File

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,9 +7,25 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     /**
-     * The application's route middleware.
+     * Global HTTP middleware stack.
      *
-     * These middleware may be assigned to groups or used individually.
+     * These middleware are run during every request.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
+    /**
+     * Route middleware.
+     *
+     * These may be assigned to routes or route groups.
      *
      * @var array
      */
@@ -17,7 +33,7 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureTokenIsValid::class,
-        'user' => \App\Http\Middleware\UserMiddleware::class, // Add this line
-        'role' => \App\Http\Middleware\RoleMiddleware::class, // I noticed you're also using a role middleware
+        'user' => \App\Http\Middleware\UserMiddleware::class,
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
     ];
 }
